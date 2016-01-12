62b76e26965f0b11370a6ad28a232daef2e89b85
mayur.badgujar <mayur.badgujar@in.ibm.com>
2016-01-12 15:20:48 +0530

    update

--- src/main/java/com/may/java/collections/Lists.java
+++ src/main/java/com/may/java/collections/Lists.java
@@ -1,11 +1,113 @@
 package com.may.java.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Lists {
-	
+
 	public static void main(String[] args) {
+
+		/**
+		 * <b> 1. ArrayList </b>
+		 * <p>
+		 * - offers constant-time positional access
+		 * </p>
+		 * <p>
+		 * - this implementation is not synchronized
+		 * </p>
+		 * 
+		 */
+		System.out.println("ArrayList :: Backed by array");
+		List<String> arrayList = new ArrayList<>();
+		arrayList.add("list1");
+		arrayList.add("list3");
+		arrayList.add("list4");
+		arrayList.add("list6");
+		arrayList.add("list2");
+		arrayList.add("list6");
+		arrayList.stream().forEach(l -> System.out.println(l));
+
+		/**
+		 * <b> 2. LinkedList </b>
+		 * <p>
+		 * - Positional access requires linear-time
+		 * </p>
+		 * <p>
+		 * - frequently add elements to the beginning of the List or iterate
+		 * over the List to delete elements from its interior
+		 * </p>
+		 * <p>
+		 * - this implementation is not synchronized
+		 * </p>
+		 * 
+		 */
+		System.out.println("LinkedList :: Backed by object references");
+		List<String> linkedList = new LinkedList<>();
+		linkedList.add("list1");
+		linkedList.add("list3");
+		linkedList.add("list4");
+		linkedList.add("list6");
+		linkedList.add("list2");
+		linkedList.add("list6");
+		linkedList.stream().forEach(System.out::println);
+		
+		
+		/**
+		 * <b> 3. Vector</b>
+		 * <p>
+		 * - offers constant-time positional access
+		 * </p>
+		 * <p>
+		 * - this implementation IS synchronized
+		 * </p>
+		 * 
+		 */
+		System.out.println("Vector :: Backed by array and synchronised");
+		List<String> vector = new Vector<>();
+		vector.add("list1");
+		vector.add("list3");
+		vector.add("list4");
+		vector.add("list6");
+		vector.add("list2");
+		vector.add("list6");
+		vector.stream().forEach(l -> System.out.println(l));
+		
+		
+		/**
+		 * <b> 5. CopyOnWriteArrayList </b>
+		 * <p>
+		 * - backed up by a copy-on-write array
+		 * </p>
+		 * <p>
+		 * - All mutative operations, such as add, set, and remove, are
+		 * implemented by making a new copy of the array i.e. an immutable
+		 * implementation
+		 * </p>
+		 * <p>
+		 * - appropriate for lists that are rarely modified but frequently
+		 * iterated.
+		 * </p>
+		 * <p>
+		 * - No synchronization required
+		 * </p>
+		 * 
+		 */
+		System.out.println("CopyOnWriteArrayList :: Backed by array, new copy of array for each modifications");
+		List<String> cpOnWriteArrayList = new CopyOnWriteArrayList<>();
+		cpOnWriteArrayList.add("list1");
+		cpOnWriteArrayList.add("list3");
+		cpOnWriteArrayList.add("list4");
+		cpOnWriteArrayList.add("list6");
+		cpOnWriteArrayList.add("list2");
+		cpOnWriteArrayList.add("list6");
+		cpOnWriteArrayList.stream().forEach(l -> System.out.println(l));
 		
 		
 		
+
 	}
 
 }
--- src/main/java/com/may/java/collections/Sets.java
+++ src/main/java/com/may/java/collections/Sets.java
@@ -1,12 +1,20 @@
 package com.may.java.collections;
 
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class Sets {
+
+	enum Day {
+		SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+	}
+
 	public static void main(String[] args) {
 
 		/**
@@ -23,7 +31,7 @@ public class Sets {
 		 * 
 		 */
 		Set<String> hashSet = new HashSet<String>();
-		System.out.println("Hashset: Standard implementation");
+		System.out.println("Hashset :: Standard implementation");
 		hashSet.add("test1");
 		hashSet.add("test2");
 		hashSet.add("test3");
@@ -48,14 +56,16 @@ public class Sets {
 		 * <b> 2. LinkedHashSet </b>
 		 * <p>
 		 * - Hash table and linked list implementation of the Set interface,
-		 * with predictable iteration order
+		 * with predictable iteration order. Can be also used as sorted set, if
+		 * elements are inserted in sorted order, will cause less overhead than
+		 * tree set
 		 * </p>
 		 * <p>
-		 * - this implementation is not synchronized
+		 * - this implementation is not synchronized.
 		 * </p>
 		 * 
 		 */
-		System.out.println("LinkedHashset: Maintain insertion order");
+		System.out.println("LinkedHashset :: Maintain insertion order");
 		Set<String> linkedHashSet = new LinkedHashSet<String>();
 		linkedHashSet.add("test1");
 		linkedHashSet.add("test2");
@@ -92,7 +102,7 @@ public class Sets {
 		 * </p>
 		 * 
 		 */
-		System.out.println("TreeSet: Sorted");
+		System.out.println("TreeSet :: Sorted");
 		Set<String> treeSet = new TreeSet<String>();
 		treeSet.add("test2");
 		treeSet.add("test1");
@@ -103,6 +113,54 @@ public class Sets {
 		treeSet.add("test5");
 		treeSet.add("test6");
 		treeSet.stream().forEach(str -> System.out.println(str));
+
+		/**
+		 * <b> 4. EnumSet </b>
+		 * <p>
+		 * - Set implementation for enum types. All of the members of an enum
+		 * set must be of the same enum type
+		 * </p>
+		 * <p>
+		 * - Enum sets support iteration over ranges of enum types
+		 * </p>
+		 * 
+		 */
+		System.out.println("EnumSet :: Set of enum :P");
+		EnumSet<Day> enumSet = EnumSet.range(Day.MONDAY, Day.FRIDAY);
+		enumSet.forEach(
+				d -> System.out.println(d));
+		Iterator<Day> it4 = enumSet.iterator();
+		enumSet.add(Day.SUNDAY);
+		it4.next();
+		
+		
+
+		/**
+		 * <b> 5. CopyOnWriteArraySet </b>
+		 * <p>
+		 * - backed up by a copy-on-write array
+		 * </p>
+		 * <p>
+		 * - All mutative operations, such as add, set, and remove, are
+		 * implemented by making a new copy of the array i.e. an immutable
+		 * implementation
+		 * </p>
+		 * <p>
+		 * - appropriate for sets that are rarely modified but frequently
+		 * iterated.
+		 * </p>
+		 * <p>
+		 * - No sychronization required
+		 * </p>
+		 * 
+		 */
+		System.out.println("CopyOnWriteArraySet :: immutable set");
+		Set<String> cpOnWrSet = new CopyOnWriteArraySet<String>();
+		cpOnWrSet.add("cpwritetest1");
+		cpOnWrSet.add("cpwritetest3"); //New copy of array for each insert, removal
+		cpOnWrSet.add("cpwritetest4");
+		cpOnWrSet.add("cpwritetest2");
+		cpOnWrSet.stream().forEach(str -> System.out.println(str));
 
 	}
 
